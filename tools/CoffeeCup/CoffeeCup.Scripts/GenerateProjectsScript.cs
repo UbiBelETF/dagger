1b1610a97632d1e4f608fccced23bca0c700b354
Harry <harry.cpp@outlook.com>
2020-10-30 22:33:13 +0100

    Sleep is a weak substitute for coffee... so take a coffee cup!

---
+++ tools/CoffeeCup/CoffeeCup.Scripts/GenerateProjectsScript.cs
@@ -0,0 +1,12 @@
+﻿using CoffeeCup.Core;
+
+namespace CoffeeCup.Scripts
+{
+    class GenerateProjectsScript : Script
+    {
+        public override void Run()
+        {
+            ScriptUtil.Run(@"tools/SharpMake/bin/Sharpmake.Application.exe /sources(@""./tools/BuildScripts/main.sharpmake.cs"")");
+        }
+    }
+}
