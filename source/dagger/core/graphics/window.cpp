4fb64f104170133c22aa1bafd14d35ef33faf28c
DaniilPrivalov <iconbitpro@gmail.com>
2020-10-30 16:15:52 +0100

    shader fix: z order checks

--- source/dagger/core/graphics/window.cpp
+++ source/dagger/core/graphics/window.cpp
@@ -232,6 +232,9 @@ void WindowSystem::SpinUp()
 
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
 }
 
 void WindowSystem::Run()
