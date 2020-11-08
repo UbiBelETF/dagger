5baa07d94908434baad3936a0632004649b5828a
DESKTOP-TPM8KBG\Johny <nikola97@gmail.com>
2020-11-08 18:04:35 +0100

    Better readability

--- source/dagger/gameplay/ping_pong/player_scores.cpp
+++ source/dagger/gameplay/ping_pong/player_scores.cpp
@@ -14,41 +14,45 @@ Float32 PlayerScoresSystem::s_TileSize = 1.f;
 
 void PlayerScoresSystem::Run()
 {
-    auto view = Engine::Registry().view<PingPongBall, Transform>();
+	auto view = Engine::Registry().view<PingPongBall, Transform>();
 
-    int ballOnField = 0;
+	int ballOnField = 0;
 
-    for (auto entity : view)
-    {
-        auto &ball = view.get<PingPongBall>(entity);
-        auto &t = view.get<Transform>(entity);
+	for (auto entity : view)
+	{
+		auto& ball = view.get<PingPongBall>(entity);
+		auto& t = view.get<Transform>(entity);
 
-        if (ball.reachedGoal && !ball.processed)
-        {
-            ball.speed = { 0,0,0 };
+		if (ball.reachedGoal && !ball.processed)
+		{
+			ball.speed = { 0,0,0 };
 
-            if (ball.playerOneScored)
-            {
-                m_goalsPlayerOne++;
-                t.position = { (-(s_FieldWidth+3) / 2.f) * s_TileSize,  s_TileSize * m_goalsPlayerOne, 0 };
-            }
-            else
-            {
-                m_goalsPlayerTwo++;
-                t.position = { ((s_FieldWidth+3) / 2.f) * s_TileSize, s_TileSize * m_goalsPlayerTwo, 0 };
-            }
+			if (ball.playerOneScored)
+			{
+				m_goalsPlayerOne++;
+				t.position = { (-(s_FieldWidth + 3) / 2.f) * s_TileSize,  s_TileSize * m_goalsPlayerOne, 0 };
+			}
+			else
+			{
+				m_goalsPlayerTwo++;
+				t.position = { ((s_FieldWidth + 3) / 2.f) * s_TileSize, s_TileSize * m_goalsPlayerTwo, 0 };
+			}
 
-            ball.processed = true;
-        }
+			ball.processed = true;
+		}
 
-        if (!ball.reachedGoal)
-        {
-            ballOnField++;
-        }
-    }
+		if (!ball.reachedGoal)
+		{
+			ballOnField++;
+		}
+	}
 
-    if (ballOnField == 0)
-    {
-        CreatePingPongBall(s_TileSize, ColorRGBA(1, 1, 1, 1), { (1 - 2 * (rand() % 2)) * (rand()%10 + 4),(rand()%10 + 4)*(1 - 2 * (rand() % 2)),0 },   { 0,rand()%(s_FieldHeight / 2),0 });
-    }
+	if (ballOnField == 0)
+	{
+		int xMultiplier = (1 - 2 * (rand() % 2));
+		int yMultiplier = (1 - 2 * (rand() % 2));
+		int xSpeed = xMultiplier * (rand() % 10 + 4);
+		int ySpeed = yMultiplier * (rand() % 10 + 4);
+		CreatePingPongBall(s_TileSize, ColorRGBA(1, 1, 1, 1), { xSpeed,ySpeed,0 }, { 0,rand() % (s_FieldHeight / 2),0 });
+	}
 }
