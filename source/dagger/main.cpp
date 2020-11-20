b586b6c064842fcf901c499c335654d2c623dc8a
DESKTOP-TPM8KBG\Johny <nikola97@gmail.com>
2020-11-20 21:21:00 +0100

    Help commit for a bug

--- source/dagger/main.cpp
+++ source/dagger/main.cpp
@@ -9,6 +9,7 @@ int main(int argc_, char** argv_)
 {
 	dagger::Engine engine;
 //	return engine.Run<team_game::TeamGame>();
+
 	return engine.Run<ping_pong::PingPongGame>();
 //	return engine.Run<racing_game::RacingGame>();
 //	return engine.Run<platformer::Platformer>();
