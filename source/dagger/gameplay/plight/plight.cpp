148e6e2de957b25748b667cffb10e34d895249ab
DESKTOP-MAATUL0\Aleksandar <maleksandar012@gmail.com>
2020-11-25 12:06:28 +0100

    Aiming System base

--- source/dagger/gameplay/plight/plight.cpp
+++ source/dagger/gameplay/plight/plight.cpp
@@ -17,6 +17,7 @@
 #include "gameplay/plight/topdown_controller.h"
 #include "gameplay/plight/plight_combat.h"
 #include "gameplay/plight/plight_collisions.h"
+#include "gameplay/plight/plight_aiming.h"
 
 
 using namespace dagger;
@@ -32,6 +33,7 @@ struct PlightCharacter
     PlightCollision& col;
     Transform& transform;
     CombatStats& cstats;
+    PlightCrosshair& crosshair;
 
     static PlightCharacter Get(Entity entity_)
     {
@@ -43,7 +45,8 @@ struct PlightCharacter
         auto& col = reg.get_or_emplace<PlightCollision>(entity_);
         auto& transform = reg.get_or_emplace<Transform>(entity_);
         auto& cstats = reg.get_or_emplace<CombatStats>(entity_);
-        return PlightCharacter{ entity_, sprite, anim, input, character ,col,transform,cstats};
+        auto& crosshair = reg.get_or_emplace<PlightCrosshair>(entity_);
+        return PlightCharacter{ entity_, sprite, anim, input, character ,col,transform,cstats,crosshair};
     }
 
     static PlightCharacter Create(
@@ -72,15 +75,21 @@ struct PlightCharacter
 
         chr.character.speed = 100;
 
+        chr.crosshair.crosshairSprite = reg.create();
+        auto& crosshairSprite = reg.emplace<Sprite>(chr.crosshair.crosshairSprite);
+        AssignSpriteTexture(crosshairSprite, "Plight:crosshair:crosshair");
+        crosshairSprite.position.x = chr.sprite.position.x + 20;
+        crosshairSprite.position.y = chr.sprite.position.y;
+
         return chr;
     }
 };
 
 void Plight::GameplaySystemsSetup(Engine &engine_)
 {
-    engine_.AddSystem<TopdownControllerSystem>();
+   // engine_.AddSystem<TopdownControllerSystem>();
     engine_.AddSystem<PlightCollisionsSystem>();
-    engine_.AddSystem<PlightCombatSystem>();
+    //engine_.AddSystem<PlightCombatSystem>();
 
 }
 
@@ -95,7 +104,7 @@ void Plight::WorldSetup(Engine &engine_)
     camera->position = { 0, 0, 0 };
     camera->Update();
 
-    plight::SetupWorld_CombatSystem(engine_);
+    plight::SetupWorld_AimingSystem(engine_);
 }
 
 void plight::SetupWorld(Engine &engine_)
@@ -218,6 +227,58 @@ void plight::SetupWorld_CombatSystem(Engine& engine_){
     frontStaminaSprite2.size = { 50, 5 };
     frontStaminaSprite2.scale = { 1, 1 };
     frontStaminaSprite2.position = { 100, 115, 1 };
+}
+
+void plight::SetupWorld_AimingSystem(Engine& engine_)
+{
+    setUpBackground(engine_);
+
+    auto mainChar = PlightCharacter::Create("ASDW_topdown", { 1, 1, 1 }, { -100, 0 });
+
+    auto backgroundHealthBar1 = Engine::Registry().create();
+    auto currentHealthBar1 = Engine::Registry().create();
+
+    auto backgroundStaminaBar1 = Engine::Registry().create();
+    auto currentStaminaBar1 = Engine::Registry().create();
+
+    mainChar.cstats.backgroundHealthBar = backgroundHealthBar1;
+    mainChar.cstats.currentHealthBar = currentHealthBar1;
+    mainChar.cstats.backgroundStaminaBar = backgroundStaminaBar1;
+    mainChar.cstats.currentStaminaBar = currentStaminaBar1;
+
+    auto& backgroundSprite = Engine::Registry().emplace<Sprite>(mainChar.cstats.backgroundHealthBar);
+
+    AssignSpriteTexture(backgroundSprite, "EmptyWhitePixel");
+    backgroundSprite.color = { 0, 0, 0, 1 };
+    backgroundSprite.size = { 50, 5 };
+    backgroundSprite.scale = { 1, 1 };
+    backgroundSprite.position = { -100, 125, 2 };
+
+    auto& frontSprite = Engine::Registry().emplace<Sprite>(mainChar.cstats.currentHealthBar);
+
+    AssignSpriteTexture(frontSprite, "EmptyWhitePixel");
+    frontSprite.color = { 1, 0, 0, 1 };
+    frontSprite.size = { 50, 5 };
+    frontSprite.scale = { 1, 1 };
+    frontSprite.position = { -100, 125, 1 };
+
+    auto& backgroundStaminaSprite = Engine::Registry().emplace<Sprite>(mainChar.cstats.backgroundStaminaBar);
+
+    AssignSpriteTexture(backgroundStaminaSprite, "EmptyWhitePixel");
+    backgroundStaminaSprite.color = { 0, 0, 0, 1 };
+    backgroundStaminaSprite.size = { 50, 5 };
+    backgroundStaminaSprite.scale = { 1, 1 };
+    backgroundStaminaSprite.position = { -100, 115, 2 };
+
+    auto& frontStaminaSprite = Engine::Registry().emplace<Sprite>(mainChar.cstats.currentStaminaBar);
+
+    AssignSpriteTexture(frontStaminaSprite, "EmptyWhitePixel");
+    frontStaminaSprite.color = { 0, 1, 0, 1 };
+    frontStaminaSprite.size = { 50, 5 };
+    frontStaminaSprite.scale = { 1, 1 };
+    frontStaminaSprite.position = { -100, 115, 1 };
+
+
 }
 
 
