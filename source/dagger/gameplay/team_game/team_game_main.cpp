a4d14d4a52c44cd76da8d43d63d1304520869607
SOFIJA\Sofija <sofija@filipovic.rs>
2020-11-28 22:55:59 +0100

    trying to merge

--- source/dagger/gameplay/team_game/team_game_main.cpp
+++ source/dagger/gameplay/team_game/team_game_main.cpp
@@ -19,27 +19,94 @@ void TeamGame::GameplaySystemsSetup(Engine &engine_)
     engine_.AddSystem<SimpleCollisionsSystem>();
 }
 
-void TeamGame::WorldSetup(Engine &engine_)
+// NEW !!! Name of the method must be something different from SetCamera() because that name is already used in platformer_main.cpp ??
+void SetCamera_team()
 {
-    ShaderSystem::Use("standard");
-
     auto* camera = Engine::GetDefaultResource<Camera>();
     camera->mode = ECameraMode::FixedResolution;
     camera->size = { 800, 600 };
     camera->zoom = 1;
     camera->position = { 0, 0, 0 };
     camera->Update();
+}
+
+void TeamGame::WorldSetup(Engine &engine_)
+{
+    ShaderSystem::Use("standard");
+
+    /*
+    auto* camera = Engine::GetDefaultResource<Camera>();
+    camera->mode = ECameraMode::FixedResolution;
+    camera->size = { 800, 600 };
+    camera->zoom = 1;
+    camera->position = { 0, 0, 0 };
+    camera->Update();
+    */
+    SetCamera_team();
 
     team_game::SetupWorld(engine_);
 }
 
 void team_game::SetupWorld(Engine &engine_)
 {
+    Vector2 scale(1, 1);
+
     auto& reg = engine_.Registry();
+
+    // field
+    constexpr int screenWidth = 800;
+    constexpr int screenHeight = 600;
+
+    constexpr int height = 20;
+    constexpr int width = 26;
+    constexpr float tileSize = 20.f;// / static_cast<float>(Width);
 
     float zPos = 1.f;
 
+    constexpr float Space = 0.05f;
+    for (int i = 0; i < height; i++)
     {
+        for (int j = 0; j < width; j++)
+        {
+            auto entity = reg.create();
+            auto& sprite = reg.emplace<Sprite>(entity);
+            AssignSpriteTexture(sprite, "EmptyWhitePixel");
+            sprite.size = scale * tileSize;
+
+            /*if (i % 2 != j % 2)
+            {
+                sprite.color.r = 0.4f;
+                sprite.color.g = 0.4f;
+                sprite.color.b = 0.4f;
+            }
+            else
+            {
+                sprite.color.r = 0.6f;
+                sprite.color.g = 0.6f;
+                sprite.color.b = 0.6f;
+            }
+
+            if (i == 0 || i == height - 1 || j == 0 || j == width - 1)
+            {
+                sprite.color.r = 0.0f;
+                sprite.color.g = 0.0f;
+                sprite.color.b = 0.0f;
+
+                //auto& col = reg.emplace<SimpleCollision>(entity);
+                //col.size.x = TileSize;
+                //col.size.y = TileSize;
+            }*/
+
+            auto& transform = reg.emplace<Transform>(entity);
+            transform.position.x = (0.5f + j + j * Space - static_cast<float>(width * (1 + Space)) / 2.f) * tileSize;
+            transform.position.y = (0.5f + i + i * Space - static_cast<float>(height * (1 + Space)) / 2.f) * tileSize;
+            transform.position.z = zPos;
+        }
+    }
+
+    zPos -= 1.f;
+
+    /*{
         auto entity = reg.create();
         auto& sprite = reg.emplace<Sprite>(entity);
         AssignSpriteTexture(sprite, "logos:dagger");
@@ -51,5 +118,5 @@ void team_game::SetupWorld(Engine &engine_)
 
         auto& col = reg.emplace<SimpleCollision>(entity);
         col.size = sprite.size;
-    }
+    }*/
 }
