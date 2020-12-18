0302af139752c4af0728cee8db3e16f5d44bde43
LAPTOP-90A5BTDB\urosu <urosuki.jovanovic@yahoo.com>
2020-12-18 18:41:20 +0100

    tile map progres

--- source/dagger/gameplay/tank_warfare/tank_warfare_main.cpp
+++ source/dagger/gameplay/tank_warfare/tank_warfare_main.cpp
@@ -23,9 +23,9 @@ using namespace tank_warfare;
 void TankWarfare::GameplaySystemsSetup(Engine &engine_)
 {
 	engine_.AddSystem<TilemapSystem>();
-    engine_.AddSystem<SimpleCollisionsSystem>();
+ //   engine_.AddSystem<SimpleCollisionsSystem>();
     engine_.AddSystem<TankControllerSystem>();
-    engine_.AddSystem<TankCollisionSystem>();
+  //  engine_.AddSystem<TankCollisionSystem>();
     engine_.AddSystem<CameraCenterSystem>();
     engine_.AddSystem<RocketSystem>();
     engine_.AddSystem<CollectibleSystem>();
@@ -99,7 +99,7 @@ void tank_warfare::SetupTestWorld(Engine& engine_)
 			auto entityMap = reg.create();
 			auto& sprite = reg.emplace<Sprite>(entityMap);
 			sprite.position = { 0, 0, 5 };
-			AssignSprite(sprite, "jovanovici:maps:preview2");
+			AssignSprite(sprite, "jovanovici:maps:map");
 		}
 	}
 	for (auto& entry : Files::recursive_directory_iterator("textures\\jovanovici\\maps"))
