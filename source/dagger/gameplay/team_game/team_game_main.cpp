bb336453f4755265b4824f913ef735384636b2cb
Sofija <sofija98733@gmail.com>
2020-12-19 18:01:50 +0100

    Changed the look of the level

--- source/dagger/gameplay/team_game/team_game_main.cpp
+++ source/dagger/gameplay/team_game/team_game_main.cpp
@@ -116,9 +116,20 @@ struct Player
         chr.currentLvl.room[0]="tilemaps/lab/lab.map";
         chr.currentLvl.room[1]="tilemaps/lab/hallway.map";
         chr.currentLvl.room[2]="tilemaps/lab/room2.map";
+        chr.currentLvl.room[3]="tilemaps/lab/room3.map";
+        chr.currentLvl.room[4]="tilemaps/lab/room4.map";
+        chr.currentLvl.room[5]="tilemaps/lab/room5.map";
+        chr.currentLvl.room[6]="tilemaps/lab/hallway_up.map";
+        chr.currentLvl.room[7]="tilemaps/lab/room_boss.map";
         chr.currentLvl.addons[0]="tilemaps/lab/addons_lab.map";
         chr.currentLvl.addons[1]="tilemaps/lab/addons_hallway.map";
         chr.currentLvl.addons[2]="tilemaps/lab/addons_room2.map";
+        chr.currentLvl.addons[3]="tilemaps/lab/addons_room3.map";
+        chr.currentLvl.addons[4]="tilemaps/lab/addons_room4.map";
+        chr.currentLvl.addons[5]="tilemaps/lab/addons_room5.map"; 
+        chr.currentLvl.addons[6]="tilemaps/lab/addons_hallway_up.map"; 
+        chr.currentLvl.addons[7]="tilemaps/lab/addons_boss.map"; 
+
 
 
         chr.currentLvl.legend['#'] = CreateObjectFunction("spritesheets:lab:wall_2",30,true,-1,100.0f,"",true,0);//CreateWallTop
@@ -129,17 +140,38 @@ struct Player
         chr.currentLvl.legend[':'] = CreateObjectFunction("spritesheets:lab:wall_4",30,true,-1,100.0f,"", true, 0);//CreateSideWallLeft;
         chr.currentLvl.legend['1'] = CreateObjectFunction("spritesheets:lab:wall_1",30,true,-1,100.0f,"", true, 0);//CreateWall1;
         chr.currentLvl.legend['3'] = CreateObjectFunction("spritesheets:lab:wall_3",30,true,-1,100.0f,"", true, 0);//CreateWall3;
+        chr.currentLvl.legend['5'] = CreateObjectFunction("spritesheets:lab:wall_15",30,true,-1,100.0f,"",true,0);//CreateWall15;
         chr.currentLvl.legend['0'] = CreateObjectFunction("",30,false,-1,100.0f,"",false,0);//Empty;
-        chr.currentLvl.legend['F'] = CreateObjectFunction("lab:door:lab anims_Animation 5_00",30,true,2,107.1f,"lab:door",false,0);//Door;
+        
         chr.currentLvl.legend['Q'] = CreateObjectFunction("spritesheets:lab:wall_bottom_1",30,true,-1,100.0f,"",true,0);//CreateWallBottom1;
         chr.currentLvl.legend['W'] = CreateObjectFunction("spritesheets:lab:wall_bottom_6",30,true,-1,100.0f,"",true,0);//CreateWallBottom6;
-        chr.currentLvl.legend['8'] = CreateObjectFunction("spritesheets:lab:floor_1",30,true,1,100.0f,"",false,0);//Hall
+        chr.currentLvl.legend['4'] = CreateObjectFunction("spritesheets:lab:wall_9",30,true,-1,100.0f,"", true, 0);//CreateWall9;
+        chr.currentLvl.legend['6'] = CreateObjectFunction("spritesheets:lab:wall_8",30,true,-1,100.0f,"", true, 0);//CreateWallB8;
+        
+
         chr.currentLvl.legend['Z'] = CreateObjectFunction("spritesheets:lab:wall_bottom_5_blank",30,false,-1,100.0f,"",true,0); //BlankWall
-        chr.currentLvl.legend['9'] = CreateObjectFunction("spritesheets:lab:floor_1",30,true,0,100.0f,"",false,0);//MainRoom
-        chr.currentLvl.legend['T'] = CreateObjectFunction("spritesheets:lab:tank_classic",20,true,-1,100.0f,"",false,0);
+        chr.currentLvl.legend['T'] = CreateObjectFunction("spritesheets:lab:tank_classic",20,true,-1,100.0f,"",false,0);//Tank
+        chr.currentLvl.legend['R'] = CreateObjectFunction("spritesheets:lab:tank_broken",20,true,-1,100.0f,"",false,0);//TankBroken
+        chr.currentLvl.legend['U'] = CreateObjectFunction("spritesheets:lab:tank_classic",15,true,-1,107.1f,"lab:tank",false,0);//TankBubbling
+        chr.currentLvl.legend['K'] = CreateObjectFunction("spritesheets:lab:acid_tenk",15,true,-1,107.1f,"lab:tank",false,0); //Acid tank
+        chr.currentLvl.legend['Y'] = CreateObjectFunction("spritesheets:lab:acid_tenk_spill",15,true,-1,107.1f,"lab:tank",false,0); //Acid tank spill
+
+
+        //enemy
         chr.currentLvl.legend['H'] = CreateObjectFunction("Skeleton",20,true,-1,100.0f,"skeleton:run",false,1);//enemy horizontal
         chr.currentLvl.legend['V'] = CreateObjectFunction("Skeleton", 20, true, -1, 100.0f, "skeleton:run",false, 2);//enemy vertical
         chr.currentLvl.legend['L'] = CreateObjectFunction("Skeleton", 20, true, -1, 100.0f, "skeleton:run",false, 3);//enemy follow
+
+        //Room Transition
+        chr.currentLvl.legend['8'] = CreateObjectFunction("spritesheets:lab:floor_1",30,true,1,100.0f,"",false,0);//Hall
+        chr.currentLvl.legend['9'] = CreateObjectFunction("spritesheets:lab:floor_1",30,true,0,100.0f,"",false,0);//MainRoom
+        chr.currentLvl.legend['F'] = CreateObjectFunction("lab:door:lab anims_Animation 5_00",30,true,3,107.1f,"lab:door",false,0);//Door, Room3
+        chr.currentLvl.legend['E'] = CreateObjectFunction("spritesheets:lab:floor_1",30,true,2,100.0f,"",false,0);//Room2
+        chr.currentLvl.legend['B'] = CreateObjectFunction("spritesheets:lab:floor_2",30,true,4,100.0f,"",false,0);//Room4
+        chr.currentLvl.legend['C'] = CreateObjectFunction("spritesheets:lab:floor_3",30,true,5,100.0f,"",false,0);//Room5
+        chr.currentLvl.legend['D'] = CreateObjectFunction("spritesheets:lab:floor_4",30,true,6,100.0f,"",false,0);//Hallway up
+        chr.currentLvl.legend['X'] = CreateObjectFunction("lab:door:lab anims_Animation 5_00",30,true,7,107.1f,"lab:door",false,0);//Door, Boss
+
 
         //sprite.color = ColorRGBA(1, 0.5f, 1, 1);
         TilemapLegend legend=chr.currentLvl.legend;
