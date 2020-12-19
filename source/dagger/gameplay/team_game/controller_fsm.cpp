b4cbb9d5df36ab59ff320777c076e7a261088057
Fedja Vujicic <fedjavujicic@gmail.com>
2020-12-19 19:26:47 +0100

    Fixed FSM falling state.

--- source/dagger/gameplay/team_game/controller_fsm.cpp
+++ source/dagger/gameplay/team_game/controller_fsm.cpp
@@ -23,7 +23,7 @@ void team_game::CharacterControllerFSM::Run(StateComponent& component_)
 	animationFSM.Run(anim);
 }
 
-// Idle
+
 
 void team_game::CharacterControllerFSM::Idle::Enter(CharacterControllerFSM::StateComponent& state_)
 {
@@ -35,7 +35,7 @@ DEFAULT_EXIT(team_game::CharacterControllerFSM, Idle);
 
 void team_game::CharacterControllerFSM::Idle::Run(CharacterControllerFSM::StateComponent& state_)
 {
-	auto& input = Engine::Registry().get<InputReceiver>(state_.entity);
+	auto&& [input, gravity] = Engine::Registry().get<InputReceiver, Gravity>(state_.entity);
 
 	if (EPSILON_NOT_ZERO(input.Get("run")))
 	{
@@ -43,12 +43,12 @@ void team_game::CharacterControllerFSM::Idle::Run(CharacterControllerFSM::StateC
 	}
 	if (EPSILON_NOT_ZERO(input.Get("jump")))
 	{
+		gravity.verticalCurrentSpeed = gravity.verticalInitialSpeed;
 		GoTo(ECharacterStates::InAir, state_);
 	}
 }
 
 
-// Running
 
 void team_game::CharacterControllerFSM::Running::Enter(CharacterControllerFSM::StateComponent& state_)
 {
@@ -56,18 +56,17 @@ void team_game::CharacterControllerFSM::Running::Enter(CharacterControllerFSM::S
 	((CharacterControllerFSM*)this->GetFSM())->animationFSM.GoTo(EAnimationStates::Running, anim);
 }
 
-// same as: DEFAULT_EXIT(CharacterControllerFSM, Running);
 void team_game::CharacterControllerFSM::Running::Exit(CharacterControllerFSM::StateComponent& state_)
 {}
 
 void team_game::CharacterControllerFSM::Running::Run(CharacterControllerFSM::StateComponent& state_)
 {
-	auto&& [sprite, transform, input, character] = Engine::Registry().get<Sprite, Transform, InputReceiver, team_game::PlayerCharacter>(state_.entity);
+	auto&& [sprite, transform, input, character, collider, gravity] = Engine::Registry().get<Sprite, Transform, InputReceiver, team_game::PlayerCharacter, Collider, Gravity>(state_.entity);
 
 	Float32 run = input.Get("run");
 	Float32 jump = input.Get("jump");
 
-	if (EPSILON_ZERO(jump))
+	if (EPSILON_ZERO(jump) && !collider.canGoDown)
 	{
 		if (EPSILON_ZERO(run))
 		{
@@ -81,17 +80,20 @@ void team_game::CharacterControllerFSM::Running::Run(CharacterControllerFSM::Sta
 	}
 	else
 	{
+		if (!EPSILON_ZERO(jump))
+		{
+			gravity.verticalCurrentSpeed = gravity.verticalInitialSpeed;
+		}
 		GoTo(ECharacterStates::InAir, state_);
 	}
 	
 }
 
-//BASIC Jumping, InputReceiver to differentiate player character from other characters. Will likely get removed based on the implementation
+
 
 void team_game::CharacterControllerFSM::InAir::Enter(CharacterControllerFSM::StateComponent& state_)
 {
 	auto&& [input, character, gravity, anim] = Engine::Registry().get<InputReceiver, team_game::PlayerCharacter, Gravity, AnimationFSM::StateComponent>(state_.entity);
-	gravity.verticalCurrentSpeed = gravity.verticalInitialSpeed;
 	((CharacterControllerFSM*)this->GetFSM())->animationFSM.GoTo(EAnimationStates::Jumping, anim);
 	
 }
