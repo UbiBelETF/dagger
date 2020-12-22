c1b2d9b059c1bc47f91cbf56dd137e318e232ba6
Miroslav <gavrilov.miroslav@gmail.com>
2020-12-22 23:22:38 +0100

    Added UI sprites

--- source/dagger/core/graphics/text.cpp
+++ source/dagger/core/graphics/text.cpp
@@ -6,7 +6,7 @@
 
 using namespace dagger;
 
-void Text::Set(String font_, String message_, Vector3 pos_)
+void Text::Set(String font_, String message_, Vector3 pos_, Bool ui_)
 {
 	font = font_;
 	position=pos_;
@@ -44,8 +44,9 @@ void Text::Set(String font_, String message_, Vector3 pos_)
 		auto spritesheet = cache[letter];
 		auto entity = registry.create();
 		auto& sprite = registry.emplace<Sprite>(entity);
+
+		if (ui_) sprite.UseAsUI();
 		sprite.position = { positionX - xOffsetDueToAlign, position.y, position.z };
-		
 		AssignSprite(sprite, spritesheet);
 
 		positionX += (int)(spritesheet->frame.size.x * spacing);
