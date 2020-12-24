f1763014a14147808ba25ec74a76880c34607f0c
Rastko Miocinovic <rastkomioc@gmail.com>
2020-12-24 23:36:44 +0100

    Fixed collider sizes of the player and tiles so that they function as they should.

--- source/dagger/gameplay/team_game/team_game_main.cpp
+++ source/dagger/gameplay/team_game/team_game_main.cpp
@@ -89,7 +89,7 @@ struct MainCharacter
         chr.character.speed = 100;
 
         //Collision setup:        
-        chr.col.size = { 25, 28 };
+        chr.col.size = { 16, 16 };
         chr.tm.position = { position_ , 0.0f };
         reg.emplace<CollisionType::Character>(entity);
 
