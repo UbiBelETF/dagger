f864f8409fe9bffc855f44a597c8d490a49b09ea
Aleksa Jovanovic <aleksa97srb@gmail.com>
2020-12-25 17:44:03 +0100

    Text merge fix

--- source/dagger/core/graphics/text.h
+++ source/dagger/core/graphics/text.h
@@ -19,10 +19,5 @@ struct Text
 	Vector3 position;
 	Float32 size;
 
-	inline void Set(String font_, String message_, Float32 size_ = 1)
-	{
-		Set(font_, message_, { 0, 0, 0 }, size_);
-	}
-
 	void Set(String font_, String message_, Vector3 pos_ = { 0, 0, 0}, Float32 size_ = 1, Bool ui_ = true);
 };
