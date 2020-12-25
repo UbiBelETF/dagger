446960a938a295baeb41e50d447df257eed1ea85
Aleksa Jovanovic <73743958+Aleksa-Jov@users.noreply.github.com>
2020-12-25 14:31:24 +0100

    Update text.h

--- source/dagger/core/graphics/text.h
+++ source/dagger/core/graphics/text.h
@@ -19,11 +19,10 @@ struct Text
 	Vector3 position;
 	Float32 size;
 
-	void Set(String font_, String message_, Vector3 pos_ = { 0, 0, 0 }, Bool ui_ = true);
 	inline void Set(String font_, String message_, Float32 size_ = 1)
 	{
 		Set(font_, message_, { 0, 0, 0 }, size_);
 	}
 
-	void Set(String font_, String message_, Vector3 pos_, Float32 size_ = 1);
+	void Set(String font_, String message_, Vector3 pos_ = { 0, 0, 0}, Float32 size_ = 1, Bool ui_ = true);
 };
